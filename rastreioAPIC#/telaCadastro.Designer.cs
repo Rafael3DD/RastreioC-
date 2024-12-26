f24fd88da17a5e5eacd18c249562177eeeabb766
Rafael3DD <rafacardoso67@hotmail.com>
2024-12-26 16:27:26 -0300

    primeiro commit

---
+++ rastreioAPIC#/telaCadastro.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace rastreioAPIC_
+{
+    partial class telaCadastro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nomeProduto = new System.Windows.Forms.TextBox();
+            this.codigoRastreio = new System.Windows.Forms.TextBox();
+            this.destino = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.data = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(633, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cadastrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome do produto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(471, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Código de rastreio";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // nomeProduto
+            // 
+            this.nomeProduto.Location = new System.Drawing.Point(46, 109);
+            this.nomeProduto.Name = "nomeProduto";
+            this.nomeProduto.Size = new System.Drawing.Size(172, 31);
+            this.nomeProduto.TabIndex = 3;
+            this.nomeProduto.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // codigoRastreio
+            // 
+            this.codigoRastreio.Location = new System.Drawing.Point(476, 109);
+            this.codigoRastreio.Name = "codigoRastreio";
+            this.codigoRastreio.Size = new System.Drawing.Size(183, 31);
+            this.codigoRastreio.TabIndex = 4;
+            // 
+            // destino
+            // 
+            this.destino.Location = new System.Drawing.Point(46, 275);
+            this.destino.Name = "destino";
+            this.destino.Size = new System.Drawing.Size(172, 31);
+            this.destino.TabIndex = 6;
+            this.destino.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Destino";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(471, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Data";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // data
+            // 
+            this.data.Location = new System.Drawing.Point(459, 275);
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(200, 31);
+            this.data.TabIndex = 8;
+            // 
+            // telaCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.data);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.destino);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.codigoRastreio);
+            this.Controls.Add(this.nomeProduto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "telaCadastro";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nomeProduto;
+        private System.Windows.Forms.TextBox codigoRastreio;
+        private System.Windows.Forms.TextBox destino;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker data;
+    }
+}
---
+++ rastreioAPIC#/telaPrincipal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rastreioAPIC_
+{
+    public partial class telaPrincipal : Form
+    {
+        public telaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            telaCadastro telacadastro = new telaCadastro();
+            telacadastro.ShowDialog();
+        }
+    }
+}
